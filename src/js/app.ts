83e85087424848fb1347c43efc89bda536a1af8d
Josh Uchytil <joshua.uchytil@gmail.com>
2019-06-11 15:56:51 -0600

    added key event listeners and logging

--- src/js/app.ts
+++ src/js/app.ts
@@ -12,7 +12,7 @@ class App {
 
 	public setup(): void {
 		// Any setup that is required that only runs once before game loads goes here
-
+		document.addEventListener('keydown', this.keyboardInput);
 		this.gameLoop();
 	}
 
@@ -21,6 +21,25 @@ class App {
 
 		this._game.render();
 	}
+
+	private keyboardInput(event: KeyboardEvent) {
+		// PRESS LEFT ARROW OR 'A' KEY
+		if (event.keyCode == 37 || event.keyCode == 65) {
+		   console.log("LEFT ARROW OR 'A' KEY");
+		}
+		// PRESS UP ARROW OR 'W' KEY
+		else if (event.keyCode == 38 || event.keyCode == 87) {
+			console.log("UP ARROW OR 'W' KEY");
+		}
+		// PRESS RIGHT ARROW OR 'D' KEY
+		else if (event.keyCode == 39 || event.keyCode == 68 ) {
+			console.log("RIGHT ARROW OR 'D' KEY");
+		}
+		// PRESS DOWN ARROW OR 'S' KEY
+		else if (event.keyCode == 40 || event.keyCode == 83 ) {
+			console.log("DOWN ARROW OR 'S' KEY");
+		}
+	 }
 }
 
 window.onload = () => {
