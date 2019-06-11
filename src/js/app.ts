e45e81438e95dfd842c2bc7212af75ee5c81cff3
Josh Uchytil <joshua.uchytil@gmail.com>
2019-06-11 16:43:49 -0600

    added mouseclick and moved functions to game

--- src/js/app.ts
+++ src/js/app.ts
@@ -12,8 +12,7 @@ class App {
 
 	public setup(): void {
 		// Any setup that is required that only runs once before game loads goes here
-		document.addEventListener('keydown', (e) => {this.keyboardInput(e, "keydown")});
-		document.addEventListener('keyup', (e) => {this.keyboardInput(e, "keyup")});
+		this._game.setup();
 		this.gameLoop();
 	}
 
@@ -22,29 +21,6 @@ class App {
 
 		this._game.render();
 	}
-
-	private keyboardInput(event: KeyboardEvent, type: string) {
-		// PRESS LEFT ARROW OR 'A' KEY
-		if (event.keyCode == 37 || event.keyCode == 65) {
-		   console.log("[🠘] " + type);
-		}
-		// PRESS UP ARROW OR 'W' KEY
-		else if (event.keyCode == 38 || event.keyCode == 87) {
-			console.log("[🠙] " + type);
-		}
-		// PRESS RIGHT ARROW OR 'D' KEY
-		else if (event.keyCode == 39 || event.keyCode == 68 ) {
-			console.log("[🠚] " + type);
-		}
-		// PRESS DOWN ARROW OR 'S' KEY
-		else if (event.keyCode == 40 || event.keyCode == 83 ) {
-			console.log("[🠛] " + type);
-		}
-		// PRESS SPACE BAR
-		else if (event.keyCode == 32) {
-			console.log("[_] " + type);
-		}
-	 }
 }
 
 window.onload = () => {
--- src/js/game.ts
+++ src/js/game.ts
@@ -37,4 +37,41 @@ export default class Game {
         this.ctx.font      = "normal 20pt Consolas";
         this.ctx.fillText(this.fps.toFixed(0) + " fps", this.width - 110, 40);
 	}
+
+	public setup(){
+		document.addEventListener('keydown', this.keyboardInput);
+		document.addEventListener('keyup', this.keyboardInput);
+		document.addEventListener("mousedown", this.mouseInput, false);
+	}
+
+	private mouseInput(event: MouseEvent){
+		let x: number = event.x;
+		let y: number = event.y;
+	 
+		console.log('x:' + x + ' y:' + y + ", " + event.type);
+	}
+
+	//something to test: while event.repeat == true/false
+	private keyboardInput(event: KeyboardEvent) {
+		// PRESS LEFT ARROW OR 'A' KEY
+		if (event.keyCode == 37 || event.keyCode == 65) {
+		   console.log("[🠘] " + event.type);
+		}
+		// PRESS UP ARROW OR 'W' KEY
+		else if (event.keyCode == 38 || event.keyCode == 87) {
+			console.log("[🠙] " + event.type);
+		}
+		// PRESS RIGHT ARROW OR 'D' KEY
+		else if (event.keyCode == 39 || event.keyCode == 68 ) {
+			console.log("[🠚] " + event.type);
+		}
+		// PRESS DOWN ARROW OR 'S' KEY
+		else if (event.keyCode == 40 || event.keyCode == 83 ) {
+			console.log("[🠛] " + event.type);
+		}
+		// PRESS SPACE BAR
+		else if (event.keyCode == 32) {
+			console.log("[_] " + event.type);
+		}
+	 }
 }
