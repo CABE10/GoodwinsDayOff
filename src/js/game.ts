91f8a89191bafc9992134215bced60a780c6a2bc
Josh Uchytil <joshua.uchytil@gmail.com>
2019-06-11 15:50:37 -0600

    included fps counter

--- src/js/game.ts
+++ src/js/game.ts
@@ -4,6 +4,11 @@ export default class Game {
 	private height: number = window.innerHeight;
 	private width: number = window.innerWidth;
 
+	//fps vars
+	private fps: number = 0;
+	private lastCalledTime: number;
+	private lastDisplay:number;
+
 	constructor() {
 		this.canvas = <HTMLCanvasElement>document.getElementById('canvas');
 		this.canvas.width = this.width;
@@ -12,6 +17,24 @@ export default class Game {
 	}
 
 	public render(): void {
-        console.log('rendering');
+		this.ctx.clearRect(0, 0, this.width, this.height);//clear old rendered content
+		this.calcFps();
+	}
+
+	private calcFps(): void{
+		 let delta = (performance.now() - this.lastCalledTime)/1000;
+		 this.lastCalledTime = performance.now();
+		 if (!this.lastDisplay || new Date().getTime() - 1000 > this.lastDisplay){
+			this.fps = 1/delta || 0;
+			this.lastDisplay = new Date().getTime();
+		 }
+		 if (this.fps !== 0)
+		 	this.showFPS();
+	}
+
+	private showFPS(){
+		this.ctx.fillStyle = "White";
+        this.ctx.font      = "normal 20pt Consolas";
+        this.ctx.fillText(this.fps.toFixed(0) + " fps", this.width - 110, 40);
 	}
 }
